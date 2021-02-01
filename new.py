0a738fad4e38dc2970d88f17bd24085e12b31375
aaaivanko <Ivan.Chvartatskyy@gmail.com>
2021-02-01 13:00:07 +0200

    new output, firewall

--- new.py
+++ new.py
@@ -1,5 +1,7 @@
 some_item = int(input('Plese write a PRICE FOR ITEM: '))
 clear_amount = some_item * 0.1
 
-print(f'You amount for one item is: {some_item - clear_amount}, firstly amount : {some_item}')
 
+
+print(f'You amount for one item is: {some_item - clear_amount}, firstly amount : {some_item}')
+print("Goodbuy, have a nice day!")
