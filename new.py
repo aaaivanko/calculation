2143838bd0aba9df3bc8353ded418136a6572d20
aaaivanko <Ivan.Chvartatskyy@gmail.com>
2021-02-01 11:33:20 +0200

    capitalize input massage

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-some_item = int(input('Plese write a price for item: '))
+some_item = int(input('Plese write a PRICE FOR ITEM: '))
 clear_amount = some_item * 0.1
 
 print(f'You amount for one item is: {some_item - clear_amount}')
