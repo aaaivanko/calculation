4c8b8ccfc9c0fef6537341d08d5384e894c0d1a1
aaaivanko <Ivan.Chvartatskyy@gmail.com>
2021-02-01 11:20:59 +0200

    changing the name of input

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-some_item = int(input('Plese write a price: '))
+some_item = int(input('Plese write a price for item: '))
 clear_amount = some_item * 0.1
 
 print(f'You amount for one item is: {some_item - clear_amount}')
